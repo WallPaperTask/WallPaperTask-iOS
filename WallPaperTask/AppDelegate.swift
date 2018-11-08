7f4000b5137803255dfd307c8e95f82a60d80ab9
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-09 01:27:12 +0900

    Change NavBar

--- WallPaperTask/AppDelegate.swift
+++ WallPaperTask/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		FirebaseApp.configure()
+        UINavigationBar.appearance().barTintColor = UIColor(hex: "00bfff")
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
 		return true
 	}
 	
